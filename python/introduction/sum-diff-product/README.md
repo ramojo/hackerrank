82fbd90e86874970f0e98b33ceb02b03b18ddc26
Allan Ramogo <amramogo@gmail.com>
2023-02-07 18:03:06 +0300

    Print the sum, difference and product of two integers on seperate lines

---
+++ python/introduction/sum-diff-product/README.md
@@ -0,0 +1,1 @@
+[Print the sum, difference and product of two integers on different lines](https://www.hackerrank.com/challenges/python-arithmetic-operators/problem)
---
+++ python/introduction/sum-diff-product/sum-diff-prod.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    sum = a+b
+    difference = a-b
+    product = a*b
+    
+    print(sum, difference, product, sep= '\n')
